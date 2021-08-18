2d0a22d8b3b018be3b94343e9bf1357583420a26
LSanchez17 <galaxyplayeremail@gmail.com>
2021-08-17 23:34:24 -0400

    preliminary model layout done

--- plantkeeper/plantkeeperapi/models.py
+++ plantkeeper/plantkeeperapi/models.py
@@ -3,7 +3,43 @@ from django.db import models
 # Create your models here.
 # migrate if you make changes here 
 class User(models.Model):
-    name = models.CharField(max_length=60)
+    """ User Model """
+    id = ''
+    username = ''
+    email = ''
+    first_name = ''
+    last_name = ''
+    profile_pic_url = ''
+    location = ''
+    password = ''
+    fully_set_up = ''
+    private = ''
+    friend_set = ''
+    friend_requests = ''
+
+
 
     def __str__(self):
         return self.name
+
+
+class Plants(models.Model):
+    """ Plants model """
+    id = ''
+    plant_name = ''
+    plant_birthday = ''
+    last_water = ''
+    last_trim = ''
+    last_repot = ''
+    indoor = ''
+    notes = ''
+    plant_pic = ''
+
+
+class Weather(models.Model):
+    """ Weather model """
+
+    location = ''
+    date = ''
+    forecast = ''
+    
