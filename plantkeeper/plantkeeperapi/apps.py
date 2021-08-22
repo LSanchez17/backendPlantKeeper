70996dd547bd073d4c9f5574435d26068a86df71
LSanchez17 <galaxyplayeremail@gmail.com>
2021-08-21 22:31:49 -0400

    rebooting django, something broke within the config loading

--- plantkeeper/plantkeeperapi/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-from django.contrib import admin
-from .models import User
-
-admin.site.register(User)
-
-class PlantkeeperapiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'plantkeeperapi'
