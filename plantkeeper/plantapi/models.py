7d4c1cf05fce95b9a455ae121f813ef0af9cece0
LSanchez17 <galaxyplayeremail@gmail.com>
2021-08-31 23:39:17 -0400

    one model added, testing, hope it doesn't break django again

--- plantkeeper/plantapi/models.py
+++ plantkeeper/plantapi/models.py
@@ -3,3 +3,12 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     id = models.CharField(primary_key=True, max_length=30)
+    username = models.CharField(max_length=30)
+    email = models.EmailField(max_length=40)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    profile_pic_url = models.URLField()
+    location = models.IntegerField()
+    password = models.CharField(max_length=500)
+    fully_set_up = models.BooleanField()
+
